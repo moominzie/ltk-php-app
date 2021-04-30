06dba0a8338f2b91f035a95dd12f286b9f1d515a
moominzie <yammybbraimong@gmail.com>
2021-04-30 13:59:44 +0700

    Change db

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,9 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','LTK_Form');
+define('DB_HOST','us-cdbr-east-03.cleardb.com');
+define('DB_USER','b981ea615185df');
+define('DB_PASS','dc16d4d6');
+define('DB_NAME','heroku_c320b9bbaf46e14');
 // Establish database connection.
 try
 {
